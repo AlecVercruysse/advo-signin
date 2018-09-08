9be9156ccb03d9cd3bd44ac553a9069960754ed5
AlecVercruysse <vercr@DESKTOP-I58C9T2.attlocal.net>
2018-09-07 21:57:33 -0700

    email sending function and initial testing. made email address and got an email to send with correct info! That was with a forced send_mail() call, still need to check if it will do it automatically

--- signin.py
+++ signin.py
@@ -4,31 +4,55 @@ import datetime
 import time
 import _thread
 import threading
+import smtplib
+from email.mime.text import MIMEText
 
 advocate_email = "alec.vercruysse@menloschool.org"
 pwd_file = "pwd.txt"
 
 
-def send_email(people_checked_in, total_people):
+def send_email(people_checked_in, total_people, date):
     print("sending email")
+    msg = "Hello, \n\n It looks like " + str(len(people_checked_in)) + " people checked in today. \n\nAttendance:\n\n"
+    for person in total_people:
+        msg += str(person) + ":\t" + ("PRESENT" if person in people_checked_in else "ABSENT") + "\n"
+    msg = MIMEText(msg)
+    msg['Subject'] = "Attendance: " + str(date)
+    bot_email_info = open(pwd_file, "r").readlines()
+    msg['From'] = bot_email_info[0]
+    msg['To'] = advocate_email
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(bot_email_info[0], bot_email_info[1])
+        server.sendmail(bot_email_info[0], advocate_email, msg.as_string())
+        server.close()
+        print("email successfully sent")
+    except:
+        print("failed to send mail")
 
 
 def daily_loop(uid_pairs):
-    people_checked_in = {}
+    people_checked_in = set()
     date_today = datetime.date.today()
     email_sent = False
     while datetime.date.today() == date_today:
-        if time.localtime().tm_hour < 15:
+        if time.localtime().tm_hour < 23:
             scan = raw_input_with_timeout()
+            if scan == "send_mail": #DEBUGGING
+                send_email(people_checked_in, [name for uid, name in uid_pairs.items()], datetime.date.today())
             if scan is not None and scan in uid_pairs:
                     people_checked_in.add(uid_pairs[scan])
-        elif not email_sent:
-            send_email(people_checked_in, [name for uid, name in uid_pairs])
+                    print(str(uid_pairs[scan]) + " checked in.")
+        elif not email_sent and len(people_checked_in) != 0:
+            send_email(people_checked_in, [name for uid, name in uid_pairs.items()], datetime.date.today())
+            email_sent = True
         else:
             continue
 
 
-def raw_input_with_timeout(prompt, timeout=30.0):
+def raw_input_with_timeout(timeout=30.0):
     """
     Uses threading. taken from
     https://gist.github.com/atupal/5865214
@@ -41,7 +65,7 @@ def raw_input_with_timeout(prompt, timeout=30.0):
     astring = None
     try:
         timer.start()
-        astring = input(prompt)
+        astring = input()
     except KeyboardInterrupt:
         pass
     timer.cancel()
@@ -58,8 +82,10 @@ def setup_attendance(population):
     for attendee in population:
         print(str(attendee) + ": please scan chip")
         uid = sys.stdin.readline()
-        uid_pairs[uid] = attendee
+        uid_pairs[uid.rstrip()] = attendee.rstrip()
+    print("Setup done. Starting bot.\nRecipient: " + str(advocate_email))
     return uid_pairs
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -67,7 +93,7 @@ if __name__ == "__main__":
     else:
         print("err: no name file specified")
         quit()
-    names = open(sys.argv[1]).readLines()
+    names = open(sys.argv[1], "r").readlines()
     uid_pairs = setup_attendance(names)
     while True:
         daily_loop(uid_pairs)
